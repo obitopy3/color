faffbda3e62398eff69ba1f499c8035b747dee6d
obitopy3 <hijaziabd63@gmail.com>
2024-08-31 00:19:14 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+	name= 'obito',
+	version= '0.1',
+	author= 'Uchiha-Obito',
+	description= 'simple color library',
+	packages=setuptools.find_packages(),
+	classifiers=[
+	"Programming Language :: python :: 3",
+	"Operating System :: OS Independent",
+	"License :: OSI Approved :: MiIT License",
+	]
+)
